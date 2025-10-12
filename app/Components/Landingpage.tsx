0073994e152f992b2effa5c8a1876f974643fb6f
Anurag Raj <adityanv8@gmail.com>
2025-10-12 16:30:59 +0530

    first commit

---
+++ app/Components/Landingpage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landingpage = () => {
+  return (
+    <div className=' flex' >
+      <div className=' lg:w-96 lg:h-screen lg:bg-yellow-300'></div>
+      <div className=' lg:w-full lg:h-screen lg:bg-green-400'>
+      </div>
+    </div>
+  )
+}
+
+export default Landingpage
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,184 @@
-import Image from "next/image";
+// 'use client'
+// import {gsap} from 'gsap'
+// import { useGSAP } from '@gsap/react'
+// import Image from 'next/image'
+// import you from './assets/youtube-color-svgrepo-com (1).svg'
+// import { useRef } from 'react'
+// export default function Page() {
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+//   const blockRef = useRef<HTMLDivElement | null>(null)
+//   const lineRef = useRef<HTMLDivElement | null>(null)
+//   const logoref = useRef<HTMLDivElement | null>(null)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+//   useGSAP(()=>{
+//     const tl = gsap.timeline({
+//       onComplete:()=>{
+
+//       }
+//     })
+//     tl.fromTo(lineRef.current,{
+//       width:0
+//     },{
+//       width:"60%",
+//       duration:1.5,
+//       ease:'power2.out'
+//     })
+
+//     tl.fromTo(lineRef.current,{
+//       width:'60%'
+//     },{
+//       width:0,
+//       duration:1.8,
+//       ease:'power2.out',
+//       onComplete:()=>{
+//         if (lineRef.current) {
+//           lineRef.current.style.display='none'
+//         }
+//       }
+//     },"-=0.5")
+
+//     tl.fromTo(logoref.current,{
+//       scale:0.8,opacity:0
+//     },{ 
+//       scale:1.2,
+//       opacity:1,
+//       duration:1.4,
+//       ease:'power2.out'
+//     })
+
+//     tl.to('',{ duration:1})
+
+//   })
+
+//   return <div>
+//     <div ref={blockRef} className=' w-full h-screen flex justify-center flex-col  items-center fixed inset-0'>
+//       <div ref={lineRef} className=' bg-red-700 w-0 h-1'></div>
+//       <div ref={logoref} className=' opacity-0'>
+//         <Image src={you} width={100} alt='youtube'></Image>
+//       </div>
+//     </div>
+//     {/* <main>Welcome to Youtube</main> */}
+//   </div>
+// }
+
+// 'use client'
+// import {gsap} from 'gsap'
+// import { useGSAP } from '@gsap/react'
+// import Image from 'next/image'
+// import you from './assests/youtube-color-svgrepo-com.svg'
+// import { useRef } from 'react'
+// export default function Page() {
+
+//   const blockRef = useRef<HTMLDivElement | null>(null)
+//   const lineRef = useRef<HTMLDivElement | null>(null)
+//   const logoref = useRef<HTMLDivElement | null>(null)
+
+//   useGSAP(()=>{
+//     const tl = gsap.timeline({
+//       onComplete:()=>{
+
+//       }
+//     })
+//     tl.fromTo(lineRef.current,{
+//       width:0
+//     },{
+//       width:"60%",
+//       duration:1.5,
+//       ease:'power2.out'
+//     })
+//   })
+
+//   return <div>
+//     <div ref={blockRef} className=' w-full h-screen flex justify-center flex-col  items-center fixed inset-0'>
+//       <div ref={lineRef} className=' bg-red-500 w-0 h-1'></div>
+//       <div ref={logoref}>
+//         <Image src={you} width={100} alt='youtube'></Image>
+//       </div>
+//     </div>
+//     {/* <main>Welcome to Youtube</main> */}
+//   </div>
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+'use client'
+import {gsap} from 'gsap'
+import { useGSAP } from '@gsap/react'
+import Image from 'next/image'
+import you from './assets/sas.svg'
+import { useRef, useState } from 'react'
+import Landingpage from './Components/Landingpage'
+export default function Page() {
+
+  const blockRef = useRef<HTMLDivElement | null>(null)
+  const lineRef = useRef<HTMLDivElement | null>(null)
+  const logoref = useRef<HTMLDivElement | null>(null)
+  const [view,setView] = useState<boolean>(false) 
+
+
+  useGSAP(()=>{
+    const tl = gsap.timeline({
+      onComplete:()=>{
+        setView((prev)=>!prev)
+      }
+    })
+    tl.fromTo(lineRef.current,{
+      width:0
+    },{
+      width:"60%",
+      duration:1.5,
+      ease:'power2.out'
+    })
+
+    tl.fromTo(lineRef.current,{
+      width:'60%'
+    },{
+      width:0,
+      duration:1.5,
+      ease:'power2.out',
+      onComplete:()=>{
+        if(lineRef.current){
+          lineRef.current.style.display='none'
+        }
+      }
+    },'-=0.5')
+
+    tl.fromTo(logoref.current,{
+      scale:0.8,
+      opacity:0
+    },{scale:1.2,
+      opacity:1,
+      duration:1.4,
+      ease:'power2.out'
+    })
+
+    tl.to({},{duration:1})
+
+    tl.to(blockRef.current,{
+      opacity:0,
+      onComplete:()=>{
+        if (blockRef.current) {
+          blockRef.current.style.display='none'
+        }
+      }
+    })
+  },[])
+
+  return( 
+  <div>
+    <div ref={blockRef} className=' w-full h-screen flex justify-center flex-col  items-center fixed inset-0'>
+      <div ref={lineRef} className=' bg-red-700 w-0 h-1'></div>
+      <div ref={logoref} className=' opacity-0'>
+        <Image src={you} width={100} alt='youtube'></Image>
+      </div>
     </div>
-  );
+    <div>
+      {
+        view && (<div>
+          <Landingpage></Landingpage>
+        </div>)
+      }
+    </div>
+  </div>
+  )
 }
